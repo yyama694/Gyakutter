12725f714b30285fff3396dcd9dfedd9160fb653
yyama <dummy@ne.jp>
2019-04-30 09:28:04 +0900

    通常ツイートを別コンポーネントに移動

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -75,28 +75,7 @@
             </div>
 
             <div class="columns  is-mobile" v-if="!d.retweeted_status">
-              <!-- 通常のツイート -->
-              <div class="column is-1-desktop is-1-tablet is-2-mobile">
-                <figure class="image">
-                  <img
-                    clss="is-rounded"
-                    style="border-radius:50%"
-                    :src="d.user.profile_image_url_https"
-                  />
-                </figure>
-              </div>
-              <div class="column">
-                <span class="is-pulled-left has-text-black">
-                  {{ d.user.name
-                  }}<span class="has-text-grey is-size-7"
-                    >@{{ d.user.screen_name }}</span
-                  >
-                </span>
-                <span class="is-pulled-right has-text-grey is-size-7">
-                  {{ formatDate(d.created_at) }}
-                </span>
-                <div class="tweet-text">{{ d.text }}</div>
-              </div>
+              <normal-tweet :data="d" />
             </div>
             <div class="columns  is-mobile" v-else>
               <!-- 引用リツイート -->
@@ -133,10 +112,12 @@
 <script>
 import getTweetById from "../api/Tweet.js";
 import { HalfCircleSpinner } from "epic-spinners";
+import NormalTweet from "./NormalTweet.vue";
 
 export default {
   components: {
-    HalfCircleSpinner
+    HalfCircleSpinner,
+    NormalTweet
   },
   name: "Main",
   props: {
---
+++ src/components/NormalTweet.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- 通常のツイート -->
+    <div class="column is-1-desktop is-1-tablet is-2-mobile">
+      <figure class="image">
+        <img
+          clss="is-rounded"
+          style="border-radius:50%"
+          :src="data.user.profile_image_url_https"
+        />
+      </figure>
+    </div>
+    <div class="column">
+      <span class="is-pulled-left has-text-black">
+        {{ data.user.name
+        }}<span class="has-text-grey is-size-7"
+          >@{{ data.user.screen_name }}</span
+        >
+      </span>
+      <span class="is-pulled-right has-text-grey is-size-7">
+        {{ formatDate(data.created_at) }}
+      </span>
+      <div class="tweet-text">{{ data.text }}</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
